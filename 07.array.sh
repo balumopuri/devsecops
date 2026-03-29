f4a361270457aae52ecdfe1ce036719aaa41de7b
Balu Mopuri <balumopuri19.com>
2026-03-29 16:30:01 +0530

    updated

--- 07.array.sh
+++ 07.array.sh
@@ -5,5 +5,6 @@ Movies=("Jalsa" "GBS" "UGS" "BRO")
 echo "First movie: ${Movies[0]}"
 echo "Second movie: ${Movies[1]}"
 echo "Third movie: ${Movies[2]}"
+echo "Fourth movie: ${Movies[2]}"
 
 echo "All movies are: ${Movies[@]}"
