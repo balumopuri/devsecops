586e7fa5c82ec1e10c31dfaa01c3aaa099d10215
Balu Mopuri <balumopuri19.com>
2026-03-29 16:40:33 +0530

    spv updated

---
+++ 08.special-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed: $@"
+echo "Number of vaiables: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home dir of current user: $HOME"
+echo "which user is running the script: $USER"
+echo "process id of the script: $$"
+sleep 60 &
+echo "process id of last command in background:$!"
