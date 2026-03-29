ef6bc6dab0cd5fd3c315c460570e05cabef85b91
Balu Mopuri <balumopuri19.com>
2026-03-29 16:16:22 +0530

    updated the script

--- 06.datatypes.sh
+++ 06.datatypes.sh
@@ -4,8 +4,7 @@ NUMBER1=$1
 NUMBER2=$2
 
 TIMESTAMP=$(date)
-echo "Script exceuted at : $TIMESTAMP"
-SUM=$(($NUMBER1+$NUMBER2))
+echo "Script executed at : $TIMESTAMP"
 
+SUM=$(($NUMBER1 + $NUMBER2))
 echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
-
