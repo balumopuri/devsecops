ec101a49ceb6d0390861396fc1ec2cb827293d52
Bala Siva Chandra Mopuri <bala.mopuri@iaggbs.com>
2026-04-09 21:15:52 +0100

    updated

--- 04.Variables.sh
+++ 04.Variables.sh
@@ -6,4 +6,5 @@ PERSON2=$2
 echo "$PERSON1: Hi Bro"
 echo "$PERSON2: Hi bro"
 echo "$PERSON1: How are you?"
-echo "$PERSON1: i need smoething"
+echo "$PERSON1: i need something"
+echo "$PERSON2: What you want bro?"
