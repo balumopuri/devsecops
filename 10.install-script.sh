25efc27ea8883f69f3ad077592e536e3b571ab9b
Bala Siva Chandra Mopuri <bala.mopuri@iaggbs.com>
2026-04-14 18:58:03 +0100

    updated the script

--- 10.install-script.sh
+++ 10.install-script.sh
@@ -9,24 +9,24 @@ then
 fi
 
 
-dnf install -y https://repo.mysql.com/mysql80-community-release-el9-1.noarch.rpm \
-|| dnf install -y https://repo.mysql.com/mysql80-community-release-el8-1.noarch.rpm
+# dnf install -y https://repo.mysql.com/mysql80-community-release-el9-1.noarch.rpm \
+# || dnf install -y https://repo.mysql.com/mysql80-community-release-el8-1.noarch.rpm
 
-# Install MySQL Server
-dnf install -y mysql-community-server
+# # Install MySQL Server
+# dnf install -y mysql-community-server
 
 
 
-    if [ $? -ne 0 ]
-    then 
-        echo "Installing MySQL.... Failure"
-        exit 1
-    else
-        echo "Installing MySQL.....success"
-    fi   
-else                 
-    echo "MYSQL is already installed"
-fi
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "Installing MySQL.... Failure"
+#         exit 1
+#     else
+#         echo "Installing MySQL.....success"
+#     fi   
+# else                 
+#     echo "MYSQL is already installed"
+# fi
     
 
 dnf install -y apache2
