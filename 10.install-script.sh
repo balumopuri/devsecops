ca77001b1e4cdbad6cb719ffc8f50cb277f5e270
Bala Siva Chandra Mopuri <bala.mopuri@iaggbs.com>
2026-04-14 18:23:04 +0100

    updated the script

--- 10.install-script.sh
+++ 10.install-script.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [ USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then 
     echo "Error:: you must have sudo access to execute the script"
     exit 1
@@ -12,11 +12,11 @@ dnf install mysql -y
 
 if [ $? -ne 0 ]
 then 
-    echo "Installing Msql.... Failure"
+    echo "Installing MySQL.... Failure"
     exit 1
 else
-    echo "Installing Msql.....success"
-fi               
+    echo "Installing MySQL.....success"
+fi                
 
 
 
