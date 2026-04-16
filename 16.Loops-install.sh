b0684cecd0c2cb103500e74b90768ebc96263337
Bala Siva Chandra Mopuri <bala.mopuri@iaggbs.com>
2026-04-16 06:10:20 +0100

    updated the script

--- 16.Loops-install.sh
+++ 16.Loops-install.sh
@@ -13,11 +13,13 @@ TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
 LOG_FILE_NAME="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
 VALIDATE(){
-if [ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then 
-        echo -e "${R} FAILED...${N}" >>"$LOG_FILE_NAME"
+        echo -e "${R} FAILED...${N}" >> "$LOG_FILE_NAME"
+        exit 1
     else   
         echo -e "$2 .... ${G}SUCCESS${N}" >> "$LOG_FILE_NAME"
+    fi
 }
 
 if [ $USERID -ne 0 ]
