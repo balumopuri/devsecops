af4de67711340dbdd2c0b298300ab51b564aef6c
balumopuri <balumopuri19@gmail.com>
2026-05-01 10:45:21 +0530

    service added

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -38,11 +38,11 @@ echo "Script started at: $TIMESTAMP" &>>"$LOG_FILE_NAME"
 dnf module disable nodejs -y &>>"$LOG_FILE_NAME"
 VALIDATE $? "Disabling NodeJS module"
 
-dnf enable nodejs:20 -y &>>"$LOG_FILE_NAME"
-VALIDATE $? "Enabling NodeJS 20"
-
 dnf install nodejs -y &>>"$LOG_FILE_NAME"
 VALIDATE $? "Installing NodeJS"
+
+dnf enable nodejs:20 -y &>>"$LOG_FILE_NAME"
+VALIDATE $? "Enabling NodeJS 20"
 
 useradd expense
 VALIDATE $? "Creating user 'expense'"
