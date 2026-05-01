35162b3cf7306e0973cea6dd5e0871d6bfd83d06
balumopuri <balumopuri19@gmail.com>
2026-05-01 16:46:26 +0530

    SCRIPT UPDATED

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -46,11 +46,11 @@ VALIDATE $? "Installing NodeJS"
 dnf module enable nodejs:20 -y &>>"$LOG_FILE_NAME"
 VALIDATE $? "Enabling NodeJS 20"
 
-# useradd expense
-# VALIDATE $? "Creating user 'expense'"
+useradd expense
+VALIDATE $? "Creating user 'expense'"
 
-# mkdir /app
-# VALIDATE $? "Creating /app directory"
+mkdir /app
+VALIDATE $? "Creating /app directory"
 
 dnf install unzip -y &>>"$LOG_FILE_NAME"
 VALIDATE $? "Installing unzip"
