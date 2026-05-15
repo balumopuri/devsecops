25f5c2126d6e179d67300521d6f3885309cb2e0e
Bala Siva Chandra Mopuri <bala.mopuri@iaggbs.com>
2026-05-15 13:44:53 +0100

    script re-updated

--- expense-shell/mysql.sh
+++ expense-shell/mysql.sh
@@ -139,6 +139,48 @@ mkdir -p "$LOG_FOLDER"
 
 echo "Script started at: $TIMESTAMP" &>>"$LOG_FILE_NAME"
 
+# ---------------- Root password to set ----------------
+ROOT_DB_PASS="ExpenseApp@1"
+
+# Try several methods to set the DB root password non-interactively.
+SET_ROOT_PASS() {
+    echo "Attempting to set DB root password" &>>"$LOG_FILE_NAME"
+
+    # Method 1: ALTER USER (MySQL 5.7+/8.0+, MariaDB compatible)
+    mysql -u root <<-EOSQL &>>"$LOG_FILE_NAME"
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_DB_PASS}';
+    FLUSH PRIVILEGES;
+EOSQL
+    if [ $? -eq 0 ]; then
+        VALIDATE 0 "Setting root password via ALTER USER"
+        return 0
+    fi
+
+    # Method 2: Alter plugin and set password (useful when auth_socket is in use)
+    mysql -u root <<-EOSQL &>>"$LOG_FILE_NAME"
+    ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${ROOT_DB_PASS}';
+    FLUSH PRIVILEGES;
+EOSQL
+    if [ $? -eq 0 ]; then
+        VALIDATE 0 "Setting root password and switching to mysql_native_password plugin"
+        return 0
+    fi
+
+    # Method 3: Older servers - update mysql.user directly
+    mysql -u root <<-EOSQL &>>"$LOG_FILE_NAME"
+    UPDATE mysql.user SET authentication_string=PASSWORD('${ROOT_DB_PASS}') WHERE User='root' AND Host='localhost';
+    FLUSH PRIVILEGES;
+EOSQL
+    if [ $? -eq 0 ]; then
+        VALIDATE 0 "Setting root password via direct mysql.user update"
+        return 0
+    fi
+
+    # All methods failed
+    VALIDATE 1 "Setting root password (all methods failed)"
+    return 1
+}
+
 # ---------------- OS Detection ----------------
 OS_NAME=$(grep '^NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
 echo "Detected OS: $OS_NAME" &>>"$LOG_FILE_NAME"
@@ -168,8 +210,7 @@ if [[ "$OS_NAME" == *"Amazon Linux"* ]]; then
     systemctl start mariadb &>>"$LOG_FILE_NAME"
     VALIDATE $? "Starting MariaDB service"
 
-    mysql_secure_installation --set-root-pass=ExpenseApp@1 &>>"$LOG_FILE_NAME"
-    VALIDATE $? "Setting up root password"
+    SET_ROOT_PASS &>>"$LOG_FILE_NAME"
 
 elif [[ "$OS_NAME" == *"Red Hat"* || "$OS_NAME" == *"CentOS"* ]]; then
     echo "Installing MySQL on RHEL/CentOS" &>>"$LOG_FILE_NAME"
@@ -182,6 +223,9 @@ elif [[ "$OS_NAME" == *"Red Hat"* || "$OS_NAME" == *"CentOS"* ]]; then
 
     systemctl start mysqld &>>"$LOG_FILE_NAME"
     VALIDATE $? "Starting MySQL service"
+
+    # Ensure root password is set for MySQL installs as well
+    SET_ROOT_PASS &>>"$LOG_FILE_NAME"
 
 else
     echo -e "${R}Unsupported OS detected: $OS_NAME${N}" | tee -a "$LOG_FILE_NAME"
