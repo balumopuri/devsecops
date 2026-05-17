ff715391fac7afe8c13dbd2c83316d9780c85af1
Balu mopuri <balumopuri19.com>
2026-05-17 11:10:20 +0530

    re-added

--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -53,10 +53,10 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Extracting frontend"
 
-npm install -g serve &>>$LOG_FILE_NAME
-VALIDATE $? "Installing serve package"
+npm install -g server &>>$LOG_FILE_NAME
+VALIDATE $? "Installing server package"
 
-cp /home/ec2-user/devsecops/expense-shell /etc/systemd/system/backend.service
+cp /home/ec2-user/devsecops/expense-shell/backend.service /etc/systemd/system/backend.service
 VALIDATE $? "Copying backend service" &>>$LOG_FILE_NAME
 
 systemctl restart nginx
